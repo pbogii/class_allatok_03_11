85b25caf558bc4585d514a2fc6ff4b9410d5fb7c
Palotás Boglárka <PalotasBoglarka@pg.local>
2026-03-11 07:46:55 +0100

    allat osztaly letrehozasa

---
+++ allatok.py
@@ -0,0 +1,11 @@
+class allat:
+    def __init__(self, nev_, faj_, eletkor_, elohely_, meret_):
+        self.nev = nev_
+        self.faj = faj_
+        self.eletkor = eletkor_
+        self.elohely = elohely_
+        self.meret = meret_
+
+    def __str__(self):
+        return f"{self.nev} nevű  {self.faj}, {self.eletkor} éves, élőhelye {self.elohely}, mérete {self.meret}"
+    
---
+++ main.py
@@ -0,0 +1,7 @@
+from allatok import allat
+
+allat1 = allat("Bodri", "kutya", 5, "kert", "közepes")
+allat2 = allat("Cirmi", "cica", 3, "lakás", "pici")
+
+print(allat1)
+print(allat2)
